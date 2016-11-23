3a6d250367048f949e8b392923bd67a64ed4d766
nurmala28 <malaelek77@gmail.com>
2016-11-23 14:50:19 +0700

    Add files via upload

---
+++ Upgrade_1521024021.php
@@ -0,0 +1,67 @@
+<html>
+<head>
+<title>UTS UPGRADE 1521024147</title>
+</head>
+	<body>
+	<table border='1'>
+	<tr>
+		<th rowspan="2">INPUT</th>
+		<th rowspan="5">OUTPUT</th>
+	</tr>
+	
+	
+<tr>
+	<th>A</th>
+	<th>I</th>
+	<th>U</th>
+	<th>E</th>
+	<th>O</th>
+</tr>
+
+<tr>
+
+
+	<td>
+		<?php
+		$n1="NURMALA KHOIRIZA ULFA";
+		echo $n1;
+		?>
+	</td>
+	<td>
+		<?php
+		$mwh_a = substr_count($n1,"A");
+		echo $mwh_a
+		?>
+   </td>
+   
+	<td>
+		<?php
+		$mwh_i = substr_count($n1,"I");
+		echo $mwh_i
+		?>
+	</td>
+	
+	<td>
+		<?php
+		$mwh_u = substr_count($n1,"U");
+		echo $mwh_u
+		?>
+	</td>
+	
+	<td>
+		<?php
+		$mwh_e = substr_count($n1,"E");
+		echo $mwh_e
+		?>
+	</td>
+	
+	<td>
+		<?php
+		$mwh_o = substr_count($n1,"O");
+		echo $mwh_o
+		?>
+</td>
+</tr>
+
+</body>
+</html>
